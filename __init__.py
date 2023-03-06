c195cf006edc0b995e52a5593fab6a3be47cae32
Gabriel Costa <gabrielcsilvasg@gmail.com>
2023-03-06 14:17:45 -0300

    Adicionar funcionalidade de desligamento

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import os
 
 class Shutdown(MycroftSkill):
     def __init__(self):
@@ -8,6 +8,8 @@ class Shutdown(MycroftSkill):
     @intent_file_handler('shutdown.intent')
     def handle_shutdown(self, message):
         self.speak_dialog('shutdown')
+        command = "shutdown -P now"
+        os.system(command)
 
 
 def create_skill():
